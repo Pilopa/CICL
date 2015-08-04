10bbab1f2df32faa504310f04399435e0d9755ef
Pilopa <KoRn3.keks@googlemail.com>
2015-08-04 13:35:24 +0200

    ...

--- js/levelview.js
+++ js/levelview.js
@@ -7,8 +7,8 @@ $(function() {
 		for(var j = 0; j < level.width; i++) {
 			var tileview = $(document.createElement('div'))
 				.addClass('tile')
-				.css('width', $('#field').css('width')/level.width)
-				.css('height', $('#field').css('height')/level.height)
+				.css('width', $('#field').css('width') / level.width)
+				.css('height', $('#field').css('height') / level.height)
 				.appendTo('#field');
 			if(level.playfield[i][j] != null) {
 				//tileview.css('background-image', '../images/' + level.playfield[i][j].type + '-' + level.playfield[i][j].rotation + '.png'); // lädt Bilddateien nach TILE_TYPE_XYZ-0.png
