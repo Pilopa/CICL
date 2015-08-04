8e54d0face01698c73baf5119fc41decdb852a6d
Steffen Müller <shimatsuke@yahoo.de>
2015-08-04 13:33:07 +0200

    gefixte Levelview
    Größe der Tileviews + richtige Zählung

--- js/levelview.js
+++ js/levelview.js
@@ -4,11 +4,11 @@ $(function() {
 	var stageid = x[1];
 	var level = getStages()[stageid].levels[levelid];
 	for(var i = 0; i < level.height; i++) {
-		for(var j = 0; j < level.width; i++) {
+		for(var j = 0; j < level.width; j++) {
 			var tileview = $(document.createElement('div'))
 				.addClass('tile')
-				.css('width', $('#field').css('width')/level.width)
-				.css('height', $('#field').css('height')/level.height)
+				.css('width', parseInt($('#field').css('width'))/level.width)
+				.css('height', parseInt($('#field').css('height'))/level.height)
 				.appendTo('#field');
 			if(level.playfield[i][j] != null) {
 				//tileview.css('background-image', '../images/' + level.playfield[i][j].type + '-' + level.playfield[i][j].rotation + '.png'); // lädt Bilddateien nach TILE_TYPE_XYZ-0.png
@@ -16,6 +16,7 @@ $(function() {
 			}
 		}
 	}
-	
-	
+	combulix.initialize();
+	combulix.speeches = new Speech("Dummy", undefined, function() {}, function() {});
+	combulix.slideIn();
 });
