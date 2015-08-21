aa7c2158435c9a8f7367ba345207f67f1f90b004
Steffen Müller <shimatsuke@yahoo.de>
2015-08-21 14:57:39 +0200

    ...

--- js/event.js
+++ js/event.js
@@ -1,12 +1,14 @@
 //=== Event ===
 	
-var EVENT_TYPE_PLACED = "PLACED";
-var EVENT_TYPE_SWAPPED = "SWAPPED";
-var EVENT_TYPE_REMOVED = "REMOVED";
-var EVENT_TYPE_ROTATED = "ROTATED";
-var EVENT_TYPE_DESTINATION_REACHED = "DESTINATION_REACHED";
-var EVENT_TYPE_TEST_COMPLETED = "TEST_COMPLETED";
-var EVENT_TYPE_TEST_FAILED = "TEST_FAILED";
+var EVENT_TYPE = {
+	'placed': "PLACED",
+	'swapped': "SWAPPED",
+	'removed': "REMOVED",
+	'rotated': "ROTATED",
+	'destinationreached': "DESTINATION_REACHED",
+	'testcompleted': "TEST_COMPLETED",
+	'testfailed': "TEST_FAILED",
+}
 
 //Der Parameter tile ist optional.
 function Event (type, tile, msg) {
